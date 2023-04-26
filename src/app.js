a02c3831a48671247daee346dbfe88f4820df6c5
jord1990 <jordy.hoogeboom@hotmail.nl>
2023-04-26 15:32:18 +0200

    finished a basic version of the required code

---
+++ src/app.js
@@ -0,0 +1,153 @@
+import axios from "axios";
+
+// performs 1 API call before storing the result in countriesPromise
+async function buildLocalCache() {
+    try {
+        return (await axios.get('https://restcountries.com/v3.1/all')).data;
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+async function fetchCountry(requestedCountry) {
+    // receives the promise from the cache
+    const countryArray = await countriesPromise
+    // loops through seeking out the requested country and returns its object
+    for (let i = 0; i < countryArray.length; i++) {
+        if (countryArray[i].name.common === requestedCountry) {
+            return countryArray[i]
+        }
+    }
+}
+async function fetchCurrencies(fetchedCountry) {
+    // grabs currency object and the name out of the country object
+    const { currencies } = await fetchedCountry
+    // empty list to push results too
+    const currencyArray = []
+    // Try block to catch countries that don't have a currency
+    try {
+        // loops through all the entries we got from Object entries in case of multiple currencies
+        for (let officialCurrency in Object.keys(currencies)) {
+            // stores the acquired key in a variable
+            let currencyKey = Object.keys(currencies)[officialCurrency]
+            // adds it to the array, block notation required to call a key with a variable name
+            currencyArray.push(currencies[currencyKey].name)
+        }
+        // returns an array with all official currencies
+        return currencyArray
+    } catch (e) {
+        // catches the countries without currencies returns null
+        return null
+    }
+}
+
+async function fetchSubRegion(fetchedCountry) {
+    const { subregion } = await fetchedCountry
+    // if subregion not undefined returns subregion
+    if (subregion) {
+        return subregion
+        // returns no subregion message if subregion undefined
+    } else {
+        return null
+    }
+}
+
+async function fetchCapitals(fetchedCountry) {
+    const { capital: capitalArray }= await fetchedCountry
+    // if capital not undefined returns capitals
+    if (capitalArray) {
+        return capitalArray
+        // returns no capital message if capitalArray undefined
+    } else {
+
+        return null
+    }
+}
+
+async function constructSearchResult(fetchedCountry) {
+
+    // fetches all the required data
+    const {region, population, flags: { png: flagPNG, alt }, name: { common: commonName } } = await fetchedCountry
+    const currencyArray = await fetchCurrencies(fetchedCountry)
+    const subRegion = await fetchSubRegion(fetchedCountry)
+    const capitals = await fetchCapitals(fetchedCountry)
+
+    // creates the elements for printing on page
+    const searchResult = document.createElement('div')
+    const flag = document.createElement('img')
+    const flagWrapper = document.createElement('div')
+    const countryTitle = document.createElement('h2')
+    const pTagOne = document.createElement('p')
+    const pTagTwo = document.createElement('p')
+
+    // ensures the capitals are printed back correctly
+    // if it has no official results it has null
+    if (subRegion === null) {
+        pTagOne.innerText =
+            `${commonName} is situated in (the) ${region} and has no subregion. it has a population of ${population} people.`
+    } else {
+        pTagOne.innerText =
+            `${commonName} is situated in ${subRegion}. it has a population of ${population} people.`
+    }
+
+    // adding the data to the elements
+    countryTitle.innerText = commonName
+    // sets the image
+    flag.setAttribute('src', flagPNG)
+    flag.setAttribute('alt', alt)
+    // ensures the capitals are printed back correctly
+    // if it has no official results it has null
+    if (capitals === null) {
+        pTagTwo.innerText = `${commonName} has no official capital`
+        // if more than 1 capital it correctly joins them and changes the wording
+    } else if (capitals.length > 1) {
+        pTagTwo.innerText = `The capitals are ${capitals.slice(0, -1).join(', ')} and ${ capitals.slice(-1)},`
+    } else {
+        pTagTwo.innerText = `The capital is ${capitals[0]}`
+    }
+
+    // ensures currency is printed back correctly
+    // if it has no official results it has null
+    if (currencyArray === null) {
+        pTagTwo.innerText += ` and has no official currency.`
+    } else if (currencyArray.length > 1) {
+        // if more than 1 currency joins them correctly
+        pTagTwo.innerText +=
+            ` and you can pay with the ${currencyArray.slice(0, -1).join(', ')} and the ${currencyArray.slice(-1)}`
+    } else {
+       pTagTwo.innerText += ` and you can pay with the ${currencyArray[0]}`
+    }
+
+    // adds classes to elements
+    searchResult.setAttribute('class', 'search-result')
+    flag.setAttribute('class', 'flag')
+    flagWrapper.setAttribute('class', 'flag-wrapper')
+    countryTitle.setAttribute('class', 'country-title')
+    pTagOne.setAttribute('class', 'region-p')
+    pTagTwo.setAttribute('class', 'capital-p')
+
+    // primes output location
+    const outputfield = document.getElementById('search-result')
+    // constructs the final result element and adds it to the page
+    // adds flag to the inside the flag wrapper
+    flagWrapper.appendChild(flag)
+    // adds all other elements inside the result div
+    searchResult.appendChild(flagWrapper)
+    searchResult.appendChild(countryTitle)
+    searchResult.appendChild(pTagOne)
+    searchResult.appendChild(pTagTwo)
+    // outputs the result div on the page
+    outputfield.appendChild(searchResult)
+}
+
+
+// makes a single api call storing the returned promise in a variable
+const countriesPromise = buildLocalCache()
+// eventListener for the search field
+const searchField = document.getElementById('search-field')
+const searchForm = document.getElementById('search-form')
+searchForm.addEventListener('submit', (e) => {
+    const fetchedCountry = fetchCountry(searchField.value)
+    void constructSearchResult(fetchedCountry)
+    e.preventDefault()
+})
