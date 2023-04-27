1977924b78f573fbca91b9abc3ca46c2c11d189b
jord1990 <jordy.hoogeboom@hotmail.nl>
2023-04-27 13:42:51 +0200

    refactored fetchCurrency, added displaying language support

--- src/app.js
+++ src/app.js
@@ -24,17 +24,13 @@ async function fetchCurrencies(fetchedCountry) {
     const { currencies } = await fetchedCountry
     // empty list to push results too
     const currencyArray = []
-    // Try block to catch countries that don't have a currency
+    // // Try block to catch countries that don't have a currency
     try {
-        // loops through all the entries we got from Object entries in case of multiple currencies
-        for (let officialCurrency in Object.keys(currencies)) {
-            // stores the acquired key in a variable
-            let currencyKey = Object.keys(currencies)[officialCurrency]
-            // adds it to the array, block notation required to call a key with a variable name
-            currencyArray.push(currencies[currencyKey].name)
-        }
-        // returns an array with all official currencies
+        // Iterates over the currency key extracting all key values from the objects inside before
+        // iterating over the objects inside using extracted keys. pushing the first value in each array.
+        Object.keys(currencies).forEach((key) => currencyArray.push(Object.values(currencies[key])[0]))
         return currencyArray
+        // }
     } catch (e) {
         // catches the countries without currencies returns null
         return null
@@ -64,13 +60,26 @@ async function fetchCapitals(fetchedCountry) {
     }
 }
 
+async function fetchLanguages(fetchedCountry) {
+    const { languages }= await fetchedCountry
+    // returns all languages
+    try {
+        return Object.values(languages)
+    } catch (e) {
+        // returns null if no official languages
+        return null
+    }
+}
+
+
 async function constructSearchResult(fetchedCountry) {
 
     // fetches all the required data
     const {region, population, flags: { png: flagPNG, alt }, name: { common: commonName } } = await fetchedCountry
     const currencyArray = await fetchCurrencies(fetchedCountry)
     const subRegion = await fetchSubRegion(fetchedCountry)
-    const capitals = await fetchCapitals(fetchedCountry)
+    const capitalArray = await fetchCapitals(fetchedCountry)
+    const languageArray = await fetchLanguages(fetchedCountry)
 
     // creates the elements for printing on page
     const searchResult = document.createElement('div')
@@ -79,6 +88,7 @@ async function constructSearchResult(fetchedCountry) {
     const countryTitle = document.createElement('h2')
     const pTagOne = document.createElement('p')
     const pTagTwo = document.createElement('p')
+    const languageTag = document.createElement('p')
 
     // ensures the capitals are printed back correctly
     // if it has no official results it has null
@@ -95,15 +105,16 @@ async function constructSearchResult(fetchedCountry) {
     // sets the image
     flag.setAttribute('src', flagPNG)
     flag.setAttribute('alt', alt)
+
     // ensures the capitals are printed back correctly
     // if it has no official results it has null
-    if (capitals === null) {
-        pTagTwo.innerText = `${commonName} has no official capital`
+    if (capitalArray === null) {
+        pTagTwo.innerText = `${commonName} has no official capital.`
         // if more than 1 capital it correctly joins them and changes the wording
-    } else if (capitals.length > 1) {
-        pTagTwo.innerText = `The capitals are ${capitals.slice(0, -1).join(', ')} and ${ capitals.slice(-1)},`
+    } else if (capitalArray.length > 1) {
+        pTagTwo.innerText = `The capitals are ${capitalArray.slice(0, -1).join(', ')} and ${ capitalArray.slice(-1)}.`
     } else {
-        pTagTwo.innerText = `The capital is ${capitals[0]}`
+        pTagTwo.innerText = `The capital is ${capitalArray[0]}.`
     }
 
     // ensures currency is printed back correctly
@@ -113,10 +124,20 @@ async function constructSearchResult(fetchedCountry) {
     } else if (currencyArray.length > 1) {
         // if more than 1 currency joins them correctly
         pTagTwo.innerText +=
-            ` and you can pay with the ${currencyArray.slice(0, -1).join(', ')} and the ${currencyArray.slice(-1)}`
+            ` and you can pay with the ${currencyArray.slice(0, -1).join(', ')} and the ${currencyArray.slice(-1)}.`
     } else {
-       pTagTwo.innerText += ` and you can pay with the ${currencyArray[0]}`
+       pTagTwo.innerText += ` and you can pay with the ${currencyArray[0]}.`
     }
+
+    if (languageArray === null) {
+        languageTag.innerText = `they have no official language.`
+        // if more than 1 capital it correctly joins them and changes the wording
+    } else if (languageArray.length > 1) {
+        languageTag.innerText = `They speak ${languageArray.slice(0, -1).join(', ')} and ${ languageArray.slice(-1)}.`
+    } else {
+        languageTag.innerText = `They speak ${languageArray[0]}.`
+    }
+
 
     // adds classes to elements
     searchResult.setAttribute('class', 'search-result')
@@ -125,9 +146,10 @@ async function constructSearchResult(fetchedCountry) {
     countryTitle.setAttribute('class', 'country-title')
     pTagOne.setAttribute('class', 'region-p')
     pTagTwo.setAttribute('class', 'capital-p')
+    languageTag.setAttribute('class', 'language-p')
 
     // primes output location
-    const outputfield = document.getElementById('search-result')
+    const outputField = document.getElementById('search-result')
     // constructs the final result element and adds it to the page
     // adds flag to the inside the flag wrapper
     flagWrapper.appendChild(flag)
@@ -136,8 +158,9 @@ async function constructSearchResult(fetchedCountry) {
     searchResult.appendChild(countryTitle)
     searchResult.appendChild(pTagOne)
     searchResult.appendChild(pTagTwo)
+    searchResult.appendChild(languageTag)
     // outputs the result div on the page
-    outputfield.appendChild(searchResult)
+    outputField.appendChild(searchResult)
 }
 
 
