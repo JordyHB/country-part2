28f6ec005ac6bf4aed92d7ef02a611fea004f420
jord1990 <jordy.hoogeboom@hotmail.nl>
2023-04-28 15:03:53 +0200

    performed some final touches on the code

--- src/app.js
+++ src/app.js
@@ -1,24 +1,30 @@
 import axios from "axios";
 
+
 // performs 1 API call before storing the result in countriesPromise
 async function buildLocalCache() {
     try {
         return (await axios.get('https://restcountries.com/v3.1/all')).data;
+
     } catch (e) {
         console.log(e);
     }
 }
 
+
 async function fetchCountry(requestedCountry) {
     // receives the promise from the cache
     const countryArray = await countriesPromise
     // loops through seeking out the requested country and returns its object
-    for (let i = 0; i < countryArray.length; i++) {
-        if (countryArray[i].name.common === requestedCountry) {
-            return countryArray[i]
-        }
-    }
+    const fetchedArray = countryArray
+        // filters for the requested country
+        .filter(countryArray =>  countryArray.name.common === requestedCountry)
+        // maps the found results to a new array
+        .map(fetchedCountry => fetchedCountry)
+    return fetchedArray[0]
 }
+
+
 async function fetchCurrencies(fetchedCountry) {
     // grabs currency object and the name out of the country object
     const { currencies } = await fetchedCountry
@@ -37,6 +43,7 @@ async function fetchCurrencies(fetchedCountry) {
     }
 }
 
+
 async function fetchSubRegion(fetchedCountry) {
     const { subregion } = await fetchedCountry
     // if subregion not undefined returns subregion
@@ -47,6 +54,7 @@ async function fetchSubRegion(fetchedCountry) {
         return null
     }
 }
+
 
 async function fetchCapitals(fetchedCountry) {
     const { capital: capitalArray }= await fetchedCountry
@@ -59,6 +67,7 @@ async function fetchCapitals(fetchedCountry) {
         return null
     }
 }
+
 
 async function fetchLanguages(fetchedCountry) {
     const { languages }= await fetchedCountry
@@ -74,93 +83,110 @@ async function fetchLanguages(fetchedCountry) {
 
 async function constructSearchResult(fetchedCountry) {
 
-    // fetches all the required data
-    const {region, population, flags: { png: flagPNG, alt }, name: { common: commonName } } = await fetchedCountry
-    const currencyArray = await fetchCurrencies(fetchedCountry)
-    const subRegion = await fetchSubRegion(fetchedCountry)
-    const capitalArray = await fetchCapitals(fetchedCountry)
-    const languageArray = await fetchLanguages(fetchedCountry)
-
-    // creates the elements for printing on page
-    const searchResult = document.createElement('div')
-    const flag = document.createElement('img')
-    const flagWrapper = document.createElement('div')
-    const countryTitle = document.createElement('h2')
-    const pTagOne = document.createElement('p')
-    const pTagTwo = document.createElement('p')
-    const languageTag = document.createElement('p')
-
-    // ensures the capitals are printed back correctly
-    // if it has no official results it has null
-    if (subRegion === null) {
-        pTagOne.innerText =
-            `${commonName} is situated in (the) ${region} and has no subregion. it has a population of ${population} people.`
-    } else {
-        pTagOne.innerText =
-            `${commonName} is situated in ${subRegion}. it has a population of ${population} people.`
-    }
-
-    // adding the data to the elements
-    countryTitle.innerText = commonName
-    // sets the image
-    flag.setAttribute('src', flagPNG)
-    flag.setAttribute('alt', alt)
-
-    // ensures the capitals are printed back correctly
-    // if it has no official results it has null
-    if (capitalArray === null) {
-        pTagTwo.innerText = `${commonName} has no official capital.`
-        // if more than 1 capital it correctly joins them and changes the wording
-    } else if (capitalArray.length > 1) {
-        pTagTwo.innerText = `The capitals are ${capitalArray.slice(0, -1).join(', ')} and ${ capitalArray.slice(-1)}.`
-    } else {
-        pTagTwo.innerText = `The capital is ${capitalArray[0]}.`
-    }
-
-    // ensures currency is printed back correctly
-    // if it has no official results it has null
-    if (currencyArray === null) {
-        pTagTwo.innerText += ` and has no official currency.`
-    } else if (currencyArray.length > 1) {
-        // if more than 1 currency joins them correctly
-        pTagTwo.innerText +=
-            ` and you can pay with the ${currencyArray.slice(0, -1).join(', ')} and the ${currencyArray.slice(-1)}.`
-    } else {
-       pTagTwo.innerText += ` and you can pay with the ${currencyArray[0]}.`
-    }
-
-    if (languageArray === null) {
-        languageTag.innerText = `they have no official language.`
-        // if more than 1 capital it correctly joins them and changes the wording
-    } else if (languageArray.length > 1) {
-        languageTag.innerText = `They speak ${languageArray.slice(0, -1).join(', ')} and ${ languageArray.slice(-1)}.`
-    } else {
-        languageTag.innerText = `They speak ${languageArray[0]}.`
-    }
-
-
-    // adds classes to elements
-    searchResult.setAttribute('class', 'search-result')
-    flag.setAttribute('class', 'flag')
-    flagWrapper.setAttribute('class', 'flag-wrapper')
-    countryTitle.setAttribute('class', 'country-title')
-    pTagOne.setAttribute('class', 'region-p')
-    pTagTwo.setAttribute('class', 'capital-p')
-    languageTag.setAttribute('class', 'language-p')
-
     // primes output location
-    const outputField = document.getElementById('search-result')
-    // constructs the final result element and adds it to the page
-    // adds flag to the inside the flag wrapper
-    flagWrapper.appendChild(flag)
-    // adds all other elements inside the result div
-    searchResult.appendChild(flagWrapper)
-    searchResult.appendChild(countryTitle)
-    searchResult.appendChild(pTagOne)
-    searchResult.appendChild(pTagTwo)
-    searchResult.appendChild(languageTag)
-    // outputs the result div on the page
-    outputField.appendChild(searchResult)
+    const outputField = document.getElementById('search-result');
+    // removes the previous print backs
+    outputField.innerHTML = ''
+    errorField.innerHTML = ''
+
+    try {
+        // fetches all the required data
+        const {region, population, flags: {png: flagPNG, alt}, name: {common: commonName}} = await fetchedCountry;
+        const currencyArray = await fetchCurrencies(fetchedCountry);
+        const subRegion = await fetchSubRegion(fetchedCountry);
+        const capitalArray = await fetchCapitals(fetchedCountry);
+        const languageArray = await fetchLanguages(fetchedCountry);
+
+        // creates the elements for printing on page
+        const searchResult = document.createElement('div');
+        const flag = document.createElement('img');
+        const flagTitleWrapper = document.createElement('div');
+        const flagWrapper = document.createElement('div');
+        const countryTitle = document.createElement('h2');
+        const pTagOne = document.createElement('p');
+        const pTagTwo = document.createElement('p');
+        const languageTag = document.createElement('p');
+
+        // ensures the capitals are printed back correctly
+        // if it has no official results it has null
+        if (subRegion === null) {
+            pTagOne.innerText =
+                `${commonName} is situated in (the) ${region} and has no subregion. it has a population of ${population} people.`
+        } else {
+            pTagOne.innerText =
+                `${commonName} is situated in ${subRegion}. it has a population of ${population} people.`
+        }
+
+        // adding the data to the elements
+        countryTitle.innerText = commonName
+        // sets the image
+        flag.setAttribute('src', flagPNG)
+        flag.setAttribute('alt', alt)
+
+        // ensures the capitals are printed back correctly
+        // if it has no official results it has null
+        if (capitalArray === null) {
+            pTagTwo.innerText = `${commonName} has no official capital.`
+            // if more than 1 capital it correctly joins them and changes the wording
+        } else if (capitalArray.length > 1) {
+            pTagTwo.innerText = `The capitals are ${capitalArray.slice(0, -1).join(', ')} and ${capitalArray.slice(-1)}.`
+        } else {
+            pTagTwo.innerText = `The capital is ${capitalArray[0]}.`
+        }
+
+        // ensures currency is printed back correctly
+        // if it has no official results it has null
+        if (currencyArray === null) {
+            pTagTwo.innerText += ` and has no official currency.`;
+        } else if (currencyArray.length > 1) {
+            // if more than 1 currency joins them correctly
+            pTagTwo.innerText +=
+                ` and you can pay with the ${currencyArray.slice(0, -1).join(', ')} and the ${currencyArray.slice(-1)}.`
+        } else {
+            pTagTwo.innerText += ` and you can pay with the ${currencyArray[0]}.`;
+        }
+
+        if (languageArray === null) {
+            languageTag.innerText = `they have no official language.`
+            // if more than 1 capital it correctly joins them and changes the wording
+        } else if (languageArray.length > 1) {
+            languageTag.innerText = `They speak ${languageArray.slice(0, -1).join(', ')} and ${languageArray.slice(-1)}.`
+        } else {
+            languageTag.innerText = `They speak ${languageArray[0]}.`
+        }
+
+
+        // adds classes to elements
+        searchResult.setAttribute('class', 'search-result');
+        flag.setAttribute('class', 'flag');
+        flagWrapper.setAttribute('class', 'flag-wrapper');
+        countryTitle.setAttribute('class', 'country-title');
+        pTagOne.setAttribute('class', 'region-p');
+        pTagTwo.setAttribute('class', 'capital-p');
+        languageTag.setAttribute('class', 'language-p');
+
+        // constructs the final result element and adds it to the page
+        // adds flag to the inside the flag wrapper
+        flagWrapper.appendChild(flag);
+        // adds the title and flag to their div
+        flagTitleWrapper.appendChild(flagWrapper);
+        flagTitleWrapper.appendChild(countryTitle);
+        // adds all other elements inside the result div
+        searchResult.appendChild(flagTitleWrapper);
+        searchResult.appendChild(pTagOne);
+        searchResult.appendChild(pTagTwo);
+        searchResult.appendChild(languageTag);
+        // outputs the result div on the page
+        outputField.appendChild(searchResult);
+    }
+
+    catch (e) {
+        console.log(e)
+        // clears the previous result
+        outputField.innerHTML = ''
+        // sends back an error
+        errorField.innerText = 'No country found, Try capitalizing!'
+    }
 }
 
 
@@ -169,8 +195,14 @@ const countriesPromise = buildLocalCache()
 // eventListener for the search field
 const searchField = document.getElementById('search-field')
 const searchForm = document.getElementById('search-form')
+const errorField = document.getElementById('error-field')
+
+// event listener that handles the search
 searchForm.addEventListener('submit', (e) => {
+    // finds the country in the data set
     const fetchedCountry = fetchCountry(searchField.value)
+    // hands the found country off to have a tile constructed
     void constructSearchResult(fetchedCountry)
+    // prevents refresh on submit
     e.preventDefault()
-})
+});
